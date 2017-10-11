65629aafc4c45156da6b105e3821265999f5c727
William Bittner <WilliamJohnBittner@gmail.com>
2017-10-10 23:50:08 -0400

    I did a thing

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # transvueify-plugin-typescript
-Transvueify plugin to transpile typescript 
+
+A [transvueify](https://github.com/trescenzi/transvueify) plugin which transpiles your code using TypeScript.
+
+First install `transvueify-plugin-typescript`
+
+`npm install transvueify-plugin-typescript --save-dev --g`
+
+Note the global flag - this is important.
+
+Then add it to your `transvueify.config.json`
+
+```json
+{
+  "plugins": ["transvueify-plugin-typescript"]
+}
+```
+
+Make sure to set up your `tsconfig.json` to include the compiler options you'd like
+as `transvueify-plugin-typescript` respects your `tsconfig.json`.
---
+++ index.js
@@ -0,0 +1,14 @@
+let ts = require("typescript");
+
+module.exports = function (compiledVueFile, filename) {
+  if (!compiledVueFile.script) {
+    return compiledVueFile;
+  }
+
+  let options = ts.convertCompilerOptionsFromJson(JSON.parse(require("find-config").read("tsconfig.json", { cwd: process.cwd() }))["compilerOptions"],
+    process.cwd()).options;
+  console.log("Transpiling " + filename + " using " + JSON.stringify(options));
+  const typeScriptOutput = ts.transpileModule(compiledVueFile.script.content, { compilerOptions: options });
+  compiledVueFile.script.content = typeScriptOutput.outputText;
+  return compiledVueFile;
+};
